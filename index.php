ecc98b22e44999115f886cbf77edf20b28654a63
Alec <alecshelembe@gmail.com>
2024-06-18 11:14:48 +0000

    Optimized code

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Navertica Test</title>
-</head>
-<body>
-    <?php
-    for ($i = 1; $i <= 100; $i++) {
-        $output = ($i % 3 == 0 ? ($i % 5 == 0 ? 'NAVERTICA' : 'NAV') : ($i % 5 == 0 ? 'ERTICA' : $i));
-        echo "<span style='padding-left:10px; padding-right:10px'>{$output}</span>" . ($i < 100 ? "," : "");
-    }
-    ?>
-</body>
-</html>
+<?php
+for ($i = 1; $i <= 100; $i++) {
+    $output = ($i % 3 == 0 ? ($i % 5 == 0 ? 'NAVERTICA' : 'NAV') : ($i % 5 == 0 ? 'ERTICA' : $i));
+    echo "<span style='padding-left:10px; padding-right:10px'>{$output}</span>" . ($i < 100 ? "," : "");
+}
+?>
